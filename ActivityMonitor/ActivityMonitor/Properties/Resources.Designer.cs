0e9f3687d63e998e63e9133cd19a1d810ed98b32
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-07-03 20:52:05 +0200

    Added Activity Monitor Schema.png

--- ActivityMonitor/ActivityMonitor/Properties/Resources.Designer.cs
+++ ActivityMonitor/ActivityMonitor/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace ActivityMonitor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Activity_Monitor_Schema {
+            get {
+                object obj = ResourceManager.GetObject("Activity_Monitor_Schema", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon Icon {
